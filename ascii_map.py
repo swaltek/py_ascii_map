71e828f36c64a726bfd87dba2301d92394c680a3
Marcin Swaltek <swaltek@tutanota.com>
2022-07-19 14:36:20 +0200

    added coloring to map

--- ascii_map.py
+++ ascii_map.py
@@ -1,6 +1,29 @@
-import curses
-from rasterio.transform import Affine
 from rasterio import features
+from rasterio.transform import Affine
+import curses
+import geopandas as gpd
+import libpysal as lps
+import networkx as nx
+import numpy as np
+
+def calc_colors( gdf: gpd.GeoDataFrame ):
+
+    neighbors = {}
+    for index, country in gdf.iterrows():
+        touches = np.array(gdf[gdf.geometry.touches(country['geometry'])].index)
+        overlaps = np.array(gdf[gdf.geometry.overlaps(country['geometry'])].index)
+        n = np.union1d(touches, overlaps)
+        neighbors[index] = n.tolist()
+
+    weights = lps.weights.W(neighbors, silence_warnings=True)
+    graph = weights.to_networkx()
+
+    colors_map = nx.coloring.greedy_color(graph)
+
+    color_map_s = [v for _, v in sorted(colors_map.items())]
+    colors = ["@", "%", "#", "&","$"]
+    return [colors[i] for i in color_map_s]
+
 
 stdscr = curses.initscr()
 curses.noecho()
@@ -13,21 +36,21 @@ lngRes = (180 - -180) / cols
 latRes = (-90 - 90) / rows
 transform = Affine.translation(-180 - lngRes / 2, 90 - latRes / 2) * Affine.scale(lngRes, latRes) 
 
-import geopandas as gpd
-import numpy as np
 
-geojson = gpd.read_file("./countries.geojson")
-geom = [(shapes, 65) for (_ ,shapes) in enumerate(geojson.geometry)]
+gdf = gpd.read_file("./countries.geojson")
+geom = [(shapes, index) for (index ,shapes) in enumerate(gdf.geometry)]
 
 rasterized = features.rasterize(
         geom,
         out_shape = (rows,cols),
-        fill = 32,
+        fill = 255,
         out = None,
         transform = transform,
         all_touched = False,
-        default_value = 37,
+        default_value = 0,
         dtype = np.dtype('i'))
+
+colors = calc_colors( gdf )
 
 offset = 0
 while True:
@@ -42,7 +65,9 @@ while True:
     for y in range(0, rows):
         for x in range(0, cols):
             try:
-                stdscr.addch(y, (x + offset) % cols, rasterized[y][x].item())
+                val = rasterized[y][x].item()
+                char = ' ' if val == 255 else colors[rasterized[y][x].item()]
+                stdscr.addch(y, (x + offset) % cols, char)
             except curses.error as _:
                 #curses throws error when advancing character past end
                 #lets ignore this error
@@ -57,9 +82,3 @@ while True:
     offset += 1
 
 curses.endwin()
-
-## Plot raster
-#fig, ax = plt.subplots(1, figsize = (10, 10))
-#show(rasterized, ax = ax)
-##plt.gca().invert_yaxis()
-#plt.show()
